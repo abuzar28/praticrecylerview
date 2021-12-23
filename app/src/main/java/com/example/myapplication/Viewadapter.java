92ec34e2e0129699781d2ce51eba0e2296e255cc
abuzar.bit <abuzarrehan95@gmail.com>
2021-12-24 01:02:52 +0500

    interface inflating

--- app/src/main/java/com/example/myapplication/Viewadapter.java
+++ app/src/main/java/com/example/myapplication/Viewadapter.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class Viewadapter {
     List<Data> friendsList;
     Activity mAct;
